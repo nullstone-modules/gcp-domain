c399c3209996f6f0829cb34a2b8eebde174a8af3
Gautam <5638929+savaliyagautam@users.noreply.github.com>
2021-08-03 20:48:07 +0530

    added dns managed zone (#1)

---
+++ domain.tf
@@ -0,0 +1,10 @@
+data "ns_domain" "this" {
+  stack_id = data.ns_workspace.this.stack_id
+  block_id = data.ns_workspace.this.block_id
+}
+
+resource "google_dns_managed_zone" "this" {
+  name     = data.ns_workspace.this.block_ref
+  dns_name = data.ns_domain.this.dns_name
+  labels   = data.ns_workspace.this.tags
+}
