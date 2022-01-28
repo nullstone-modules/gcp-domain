ad8d1fda1a1f09f357bb8b031e942a215bf94d14
Brad Sickles <brad.sickles@gmail.com>
2022-01-27 19:57:21 -0500

    Enable cloud resource manager

--- gcp.tf
+++ gcp.tf
@@ -4,3 +4,9 @@ locals {
   project_id     = data.google_project.this.project_id
   project_number = data.google_project.this.number
 }
+
+resource "google_project_service" "cloud_resource_manager" {
+  project            = local.project_id
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
